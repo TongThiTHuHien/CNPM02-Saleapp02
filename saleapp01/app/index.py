322fb4d3bc652c96001ef4796ff6e3f8f0ad512c
TongThiTHuHien <94513564+TongThiTHuHien@users.noreply.github.com>
2023-11-12 15:37:26 +0700

    init projects

--- saleapp01/app/index.py
+++ saleapp01/app/index.py
@@ -1,14 +1,14 @@
-from flask import render_template,request
+from flask import render_template, request
 import dao
-from app import app
+from app import app, login
 
 
 @app.route('/')
 def index():
-    kw= request.args.get('kw')
+    kw = request.args.get('kw')
 
     cates = dao.load_categories()
-    products=dao.load_products(kw=kw)
+    products = dao.load_products(kw=kw)
 
     return render_template('index.html', categories=cates, products=products)
 
@@ -18,6 +18,15 @@ def details(id):
     return render_template('details.html')
 
 
+@app.route('/admin/login', methods=['post'])
+def login_admin_process():
+    request.form.get('username')
+    request.form.get('pasword')
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 if __name__ == '__main__':
+    from app import admin
     app.run(debug=True)
--- saleapp01/app/models.py
+++ saleapp01/app/models.py
@@ -1,5 +1,16 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from app import db
+from flask_login import UserMixin
+
+
+
+class User(db.Model, UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    avatar = Column(String(100), default='https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/iPad-Pro-12.9-gray1.jpg')
 
 
 class Category(db.Model):
@@ -7,6 +18,10 @@ class Category(db.Model):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False, unique=True)
+    products= relationship('Product', backref='category', lazy=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(db.Model):
